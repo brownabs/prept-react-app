589d8b51b3a7a87b3fe367aa3b03353328f3130b
Abbey Brown <abbeybrownsemail@gmail.com>
2020-11-30 19:58:07 -0600

    half class

--- src/components/Cards/QuestionCard.js
+++ src/components/Cards/QuestionCard.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-export default function QuestionCard() {
+export default function QuestionCard({ card }) {
   return (
     <>
     <div className='d-flex flex-column m-3 w-25'>
     <h1>Question:</h1>
     <div className='card'>
       <div className='card-body'>
-        <h5 className='card-title'>""</h5>
+        <h5 className='card-title'>{card.question}</h5>
       </div>
     </div>
     </div>
--- src/helpers/data/questionData.js
+++ src/helpers/data/questionData.js
@@ -1,7 +1,11 @@
-// const baseUrl = '';
+import axios from 'axios';
+
+const baseUrl = 'https://prept-1eda9.firebaseio.com/';
 
 const getQuestions = () => new Promise((resolve, reject) => {
-
+  axios.get(`${baseUrl}.json`).then((response) => {
+    resolve(Object.values(response.data));
+  }).catch((error) => reject(error));
 });
 
 export default { getQuestions };
