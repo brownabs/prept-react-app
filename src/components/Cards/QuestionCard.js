872837f0d7b22aeeb3c51eb9dc5340aa23c903d4
Abbey Brown <abbeybrownsemail@gmail.com>
2020-11-30 20:28:57 -0600

    second part of class

--- src/components/Cards/QuestionCard.js
+++ src/components/Cards/QuestionCard.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import { Button } from 'reactstrap';
 
-export default function QuestionCard({ card }) {
+export default function QuestionCard({ card, showAnswer }) {
   return (
     <>
     <div className='d-flex flex-column m-3 w-25'>
@@ -8,6 +9,7 @@ export default function QuestionCard({ card }) {
     <div className='card'>
       <div className='card-body'>
         <h5 className='card-title'>{card.question}</h5>
+        <Button className="ml-1" id={card.firebaseKey} onClick={(e) => showAnswer(e)} >Answer</Button>
       </div>
     </div>
     </div>
--- src/views/FlashCard/index.js
+++ src/views/FlashCard/index.js
@@ -15,18 +15,34 @@ export default class FlashCard extends React.Component {
   }
 
   loadData = () => {
-    QuestionData.getQuestions().then((response) => {
-      this.setState({
-        flashCards: response,
-        currentCard: response[0],
+    if (!this.state.answer) {
+      QuestionData.getQuestions().then((response) => {
+        this.setState({
+          flashCards: response,
+          currentCard: response[0],
+        });
       });
+    } else {
+      const { flashCards } = this.state;
+      const nextQuestion = flashCards.indexOf(this.state.currentCard) + 1;
+      this.setState({
+        answer: false,
+        currentCard: flashCards[nextQuestion] || flashCards[0],
+      });
+    }
+  }
+
+  showAnswerToQuestion = (e) => {
+    e.preventDefault();
+    this.setState({
+      answer: true,
     });
   }
 
   render() {
     const { answer, currentCard } = this.state;
-    const showQuestion = () => <QuestionCard key={currentCard.firebaseKey} card={currentCard}/>;
-    const showAnswer = () => <AnswerCard key={currentCard.firebaseKey} card={currentCard}/>;
+    const showQuestion = () => <QuestionCard key={currentCard.firebaseKey} card={currentCard} showAnswer={this.showAnswerToQuestion}/>;
+    const showAnswer = () => <AnswerCard key={currentCard.firebaseKey} card={currentCard} showNextQuestion={this.loadData}/>;
     return (
         <div className="flash-card d-flex flex-wrap justify-content-center">
           {answer === false ? showQuestion() : showAnswer() }
