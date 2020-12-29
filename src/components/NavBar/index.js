18149b921be125d259b0e22980d83ee2d5e10dc9
Abbey Brown <abbeybrownsemail@gmail.com>
2020-12-28 23:22:50 -0600

    fixing conflict errors

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,35 +1,19 @@
 import React, { StrictMode } from 'react';
-<<<<<<< HEAD
 import { Link } from 'react-router-dom';
-=======
->>>>>>> c30ee86fdea1bacb5175ccfa435412c13e95598c
 import {
   Navbar,
   Nav,
   NavItem,
 } from 'reactstrap';
-<<<<<<< HEAD
-=======
-import { Link } from 'react-router-dom';
->>>>>>> c30ee86fdea1bacb5175ccfa435412c13e95598c
 
 export default function NavBar() {
   return (
       <StrictMode>
       <Navbar expand='md' className='navbar justify-content-between'>
-<<<<<<< HEAD
         <Link className="navbar-brand" to='/'>Prept</Link>
           <Nav className='mr-auto' navbar>
             <NavItem>
             <Link className="navbar-brand" to='/flash-card'>Flash Cards</Link>
-=======
-          <Nav className='mr-auto' navbar>
-            <NavItem>
-              <Link className="nav-bar" to='/'>Home</Link>
-            </NavItem>
-            <NavItem>
-              <Link className="nav-dash-bar" to='/flash-card'>Flash Cards</Link>
->>>>>>> c30ee86fdea1bacb5175ccfa435412c13e95598c
             </NavItem>
           </Nav>
           </Navbar>
--- src/helpers/Routes.js
+++ src/helpers/Routes.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import FlashCard from '../views/FlashCard';
-<<<<<<< HEAD
-import NotFound from '../views/NotFound';
-import Home from '../views/Home';
-=======
 import Home from '../views/Home';
 import NotFound from '../views/NotFound';
->>>>>>> c30ee86fdea1bacb5175ccfa435412c13e95598c
 
 export default function Routes() {
   return (
       <Switch>
         <Route
-<<<<<<< HEAD
           exact
           path='/'
           component={() => <Home/>}
@@ -25,18 +19,5 @@ export default function Routes() {
         />
         <Route component={NotFound} />
       </Switch>
-=======
-        exact
-        path='/'
-        component={() => <Home/>}
-        />
-        <Route
-        exact
-        path='/flash-card'
-        component={() => <FlashCard/>}
-        />
-        <Route component={NotFound} />
-        </Switch>
->>>>>>> c30ee86fdea1bacb5175ccfa435412c13e95598c
   );
 }
